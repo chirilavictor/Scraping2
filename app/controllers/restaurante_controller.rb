e79241f96a7c3c40190ed2900415562f47fd7dc8
ChirilaVictor <chirilavictor@yahoo.com>
2015-02-04 16:50:19 +0200

    6 commit

--- app/controllers/restaurante_controller.rb
+++ app/controllers/restaurante_controller.rb
@@ -29,27 +29,27 @@ class RestauranteController < ApplicationController
   end
 
   def extrage_meniu_10ani
-  	@var = 1423054400 
-  	while Time.now.to_i < 1738412867 #anul 2025 04 febuarie
-  		if @var == Time.now.to_i
-  			RestHora.all.each do |x|
-		  		x.destroy
-		  	end
-		  	@page = Nokogiri::HTML(open('http://www.restaurantul-hora.ro/meniu-restaurantul-hora/'))
+  	# @var = 1423054400 
+  	# while Time.now.to_i < 1738412867 #anul 2025 04 febuarie
+  	# 	if @var == Time.now.to_i
+  	# 		RestHora.all.each do |x|
+		 #  		x.destroy
+		 #  	end
+		 #  	@page = Nokogiri::HTML(open('http://www.restaurantul-hora.ro/meniu-restaurantul-hora/'))
 
-				@tabele_meniu = @page.search('table')
+			# 	@tabele_meniu = @page.search('table')
 				
-				@tabele_meniu.each do |tabel|
-					if tabel.search('tr').search('td').at_css('.menu-category') != nil 
-						@categorie = tabel.search('tr').search('td').at_css('.menu-category').text
+			# 	@tabele_meniu.each do |tabel|
+			# 		if tabel.search('tr').search('td').at_css('.menu-category') != nil 
+			# 			@categorie = tabel.search('tr').search('td').at_css('.menu-category').text
 
-						RestHora.create(categorie:@categorie)
+			# 			RestHora.create(categorie:@categorie)
 			
-					end		
-				end	
-				@var += 60		
-		  end
-		 # redirect_to root_url
-  	end
+			# 		end		
+			# 	end	
+			# 	@var += 60		
+		 #  end
+		 # # redirect_to root_url
+  	# end
 	end
 end
